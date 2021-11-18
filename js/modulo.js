868a12007d5a3d792e4c8ed4f3dd163c831b7264
FlorMezaSaavedra <mezasaavedraflor@gmail.com>
2021-11-18 11:32:24 -0600

    primer commit

---
+++ js/modulo.js
@@ -0,0 +1,13 @@
+ // 18-11-21  JUEVES
+
+ // creamos una funcion 
+ 
+ function saludo (user) {
+     //mensaje = "hola, " ;
+     //mensaje + = user:
+     //return (mensaje)
+     return (`¡Bienvenid@ les saluda, ${user} !`)
+ }
+ console.log(saludo("Flor y Keila"));
+
+ export {saludo}
---
+++ js/scrip.js
@@ -0,0 +1,7 @@
+  // 18-11-21  JUEVES
+ 
+ //llamar funcion
+
+import{saludo} from './modulo.js' ;
+
+alert(saludo("Flor y Keila :)"))
